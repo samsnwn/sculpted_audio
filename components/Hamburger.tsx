ce5844917572d2bcdf7af8d84419a7dee026ddbc
samsnwn <samuel.c.glantz@gmail.com>
2024-08-20 10:58:18 +0200

    nav

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -19,19 +19,19 @@ const HamburgerMenu = ({
       >
         {/* Top bun */}
         <span
-          className={`h-1 w-full rounded-lg transition-transform duration-300 ease-in-out bg-white ${
+          className={`h-1 w-full rounded-lg transition-transform duration-500 ease-in-out bg-white ${
             menuOpen ? "rotate-45 translate-y-3" : ""
           }`}
         />
         {/* Meat */}
         <span
-          className={`h-1 w-full rounded-lg transition-opacity duration-300 ease-in-out bg-white ${
+          className={`h-1 w-full rounded-lg transition-opacity duration-500 ease-in-out bg-white ${
             menuOpen ? "opacity-0" : ""
           }`}
         />
         {/* Bottom bun */}
         <span
-          className={`h-1 w-full rounded-lg transition-transform duration-300 ease-in-out bg-white ${
+          className={`h-1 w-full rounded-lg transition-transform duration-500 ease-in-out bg-white ${
             menuOpen ? "-rotate-45 -translate-y-3" : ""
           }`}
         />
