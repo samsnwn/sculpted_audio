d564122e3bf34b7c28c28a6593f48510e1e38c6f
samsnwn <samuel.c.glantz@gmail.com>
2024-08-20 12:13:35 +0200

    layout

--- components/sections/Portfolio.tsx
+++ components/sections/Portfolio.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div  className="flex flex-col lg:w-1/2 h-screen" id="portfolio">
+    <div  className="flex flex-col lg:w-1/2 h-screen pt-20" id="portfolio">
         <div className="w-5 h-0.5 bg-red border border-red" />
         <div className="w-0.5 bg-red h-4 border border-red" />
-        <h1 className="text-white text-6xl ml-5 md:items-start">
+        <h1 className="text-white text-4xl md:text-5xl lg:text-6xl ml-5 md:items-start">
           Portfolio
         </h1>
-        <h2 style={{fontWeight: '100'}}>We transform sound into art. Our state-of-the-art facilities and expert team are dedicated to delivering unmatched quality in audio mastering, mixing, and sound design. We elevate your sound to new heights, ensuring every detail is perfected.</h2>
   </div>
   )
 }
--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -1,9 +1,9 @@
 const Services = () => {
   return (
-    <section  className="flex flex-col lg:w-1/2 h-screen" id="services">
+    <section  className="flex flex-col lg:w-1/2 h-screen pt-20" id="services">
         <div className="w-5 h-0.5 bg-red border border-red" />
         <div className="w-0.5 bg-red h-4 border border-red" />
-        <h1 className="text-white text-6xl ml-5 md:items-start">
+        <h1 className="text-white text-4xl md:text-5xl lg:text-6xl ml-5 md:items-start">
           Services
         </h1>
   </section>
