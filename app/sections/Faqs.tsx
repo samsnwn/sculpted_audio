911aa47cb612fe8bfd0a70c43038c3533412f417
samsnwn <samuel.c.glantz@gmail.com>
2024-09-13 13:47:09 +0200

    faqs

--- app/sections/Faqs.tsx
+++ app/sections/Faqs.tsx
@@ -1,9 +1,72 @@
 import {
-  Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
+  Accordion,
 } from "@/components/ui/accordion";
+
+const faqs = [
+  {
+    question: "What audio formats do you accept for mastering/mixing?",
+    answer:
+      "Please submit files in 24-bit or 32-bit WAV format, ideally at the sample rate of your project and recordings. AIFF files are alsoaccepted. Avoid applying any limiting or clipping processes to your files.",
+  },
+  {
+    question: "In what formats will my masters/pre-masters be delivered?",
+    answer:
+      "By default, if no specific requirements are provided, we will deliver masters in the following formats: - WAV 24-bit (up to 192kHz) for distribution and streaming  - WAV 16-bit 44.1kHz for CD production (DDP format available upon request)  - MP3 320 Kbps for referencing and sharing Metadata and ISRC codes will be included if requested and provided",
+  },
+  {
+    question: "What sample rate should my pre-masters be?",
+    answer:
+      "Keep your sample rate the same as your recording and mixing project. Avoid down-sampling or up-sampling if possible.",
+  },
+  {
+    question: "How should I name my files?",
+    answer:
+      "Properly naming your files helps ensure a smooth delivery process and avoids confusion. For mastering, use the format: ‘Track Number Artist - Song Name_Mix Version_BitDepth_SampleRate’. For example: 01 SculptedAudio - My Song_V1_24b_44.1. In stem mastering, include the stem name after the track name: 01 SculptedAudio - My Song (KICKS)_V1_24b_44.1.",
+  },
+  {
+    question: "Should I choose stereo mastering or stem mastering?",
+    answer:
+      "Stereo mastering is usually the preferred method. Stem mastering is recommended only if there are issues in the mix that can’t be resolved through traditional mastering or by the mixing engineer. Note that stem mastering may alter the final mix slightly, as any mix buss processing will need to be removed. If you are not fully satisfied with your mix, we offer a free consultation to help determine the best approach for your track. If needed, we can also provide mixing services to ensure your track is in optimal shape before mastering.",
+  },
+  {
+    question: "What is your revision policy?",
+    answer:
+      "We offer unlimited revisions to ensure your satisfaction with the masters. However, submitting new mixes will incur a small fee depending on the extent of the changes.",
+  },
+  {
+    question: "How long does the mixing/mastering process take?",
+    answer:
+      "Turnaround times vary depending on the complexity and workload. Typically, mixing and mastering are completed within 7 days. For expedited services, please inquire about availability and additional fees.",
+  },
+  {
+    question: "What do I need to provide for mixing/mastering?",
+    answer:
+      "Along with your audio files, please provide any relevant notes or specific instructions about your project. For mixing, include any demo mixes, self-masters, and reference tracks that align with your vision for the final sound.",
+  },
+  {
+    question: "Can I make changes to my mix or master after delivery?",
+    answer:
+      "Yes, you can request changes to your mix or master. Please note that additional revisions beyond the initial scope may incur extra charges, depending on the nature and extent of the changes.",
+  },
+  {
+    question: "Do you offer packages or discounts for multiple tracks?",
+    answer:
+      "Yes, we offer packages and discounts for multiple tracks. Please contact us for more details and customized pricing based on your needs.",
+  },
+  {
+    question: "Can I visit the studio or have a remote session?",
+    answer:
+      "We offer both in-studio and remote sessions. Please contact us to schedule a visit or discuss remote session options.",
+  },
+  {
+    question: "Is it important to provide reference tracks or albums?",
+    answer:
+      "Yes, providing reference tracks or albums is highly desirable. They help us understand your vision and goals for the final sound by offering a benchmark for the style and quality you are aiming to achieve.",
+  },
+];
 
 const Faqs = () => {
   return (
@@ -17,117 +80,14 @@ const Faqs = () => {
         FAQs
       </h2>
       <div className="w-[90vw] m-auto py-20 max-w-[90%]">
-      <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            What audio formats do you accept for mastering/mixing?
-          </AccordionTrigger>
-          <AccordionContent>
-            Please submit files in 24-bit or 32-bit WAV format, ideally at the
-            sample rate of your project and recordings. AIFF files are also
-            accepted. Avoid applying any limiting or clipping processes to your
-            files.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>
-            In what formats will my masters/pre-masters be delivered?
-          </AccordionTrigger>
-          <AccordionContent>
-            By default, if no specific requirements are provided, we will
-            deliver masters in the following formats:
-             - WAV 24-bit (up to 192kHz) for distribution and streaming 
-             - WAV 16-bit 44.1kHz for CD production (DDP format available upon request) 
-             - MP3 320 Kbps for referencing and sharing Metadata and ISRC codes will be included if requested and provided
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>
-            What sample rate should my pre-masters be?
-          </AccordionTrigger>
-          <AccordionContent>
-            Keep your sample rate the same as your recording and mixing project.
-            Avoid down-sampling or up-sampling if possible.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-4">
-          <AccordionTrigger>How should I name my files?</AccordionTrigger>
-          <AccordionContent>
-            Properly naming your files helps ensure a smooth delivery process
-            and avoids confusion. For mastering, use the format: ‘Track Number
-            Artist - Song Name_Mix Version_BitDepth_SampleRate’. For example: 01
-            SculptedAudio - My Song_V1_24b_44.1. In stem mastering, include the
-            stem name after the track name: 01 SculptedAudio - My Song
-            (KICKS)_V1_24b_44.1.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-5">
-          <AccordionTrigger>
-            Should I choose stereo mastering or stem mastering?
-          </AccordionTrigger>
-          <AccordionContent>
-            Stereo mastering is usually the preferred method. Stem mastering is
-            recommended only if there are issues in the mix that can’t be
-            resolved through traditional mastering or by the mixing engineer.
-            Note that stem mastering may alter the final mix slightly, as any
-            mix buss processing will need to be removed. If you are not fully
-            satisfied with your mix, we offer a free consultation to help
-            determine the best approach for your track. If needed, we can also
-            provide mixing services to ensure your track is in optimal shape
-            before mastering.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-6">
-          <AccordionTrigger>What is your revision policy?</AccordionTrigger>
-          <AccordionContent>
-            We offer unlimited revisions to ensure your satisfaction with the
-            masters. However, submitting new mixes will incur a small fee
-            depending on the extent of the changes.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-7">
-          <AccordionTrigger>
-            How long does the mixing/mastering process take?
-          </AccordionTrigger>
-          <AccordionContent>
-            Turnaround times vary depending on the complexity and workload.
-            Typically, mixing and mastering are completed within 7 days. For
-            expedited services, please inquire about availability and additional
-            fees.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-8">
-          <AccordionTrigger>
-            What do I need to provide for mixing/mastering?
-          </AccordionTrigger>
-          <AccordionContent>
-            Along with your audio files, please provide any relevant notes or
-            specific instructions about your project. For mixing, include any
-            demo mixes, self-masters, and reference tracks that align with your
-            vision for the final sound.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-9">
-          <AccordionTrigger>
-            Can I make changes to my mix or master after delivery?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes, you can request changes to your mix or master. Please note that
-            additional revisions beyond the initial scope may incur extra
-            charges, depending on the nature and extent of the changes.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-10">
-          <AccordionTrigger>
-            Do you offer packages or discounts for multiple tracks?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes, we offer packages and discounts for multiple tracks. Please
-            contact us for more details and customized pricing based on your
-            needs.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+        <Accordion type="single" collapsible className="w-full">
+          {faqs.map((faq, index) => (
+            <AccordionItem value={`item-${index}`} key={index}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>{faq.answer}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
     </section>
   );
