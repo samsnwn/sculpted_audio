531b6bd9c78525413a83354c635f9f7a01492f00
samsnwn <samuel.c.glantz@gmail.com>
2024-10-17 11:32:09 +0200

    hero

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -25,10 +25,10 @@ const Hero = () => {
   const arrowOpacity = Math.max(0, 1 - scrollY / 200);
 
   return (
-    <section className="w-full min-h-screen flex items-center m-auto relative">
+    <section className="w-full min-h-screen flex m-auto relative">
       {/* <GridBackground className="text-neutral-700 -z-50" /> */}
       <SectionWrapper>
-        <div className="flex flex-col md:flex-row items-center justify-between gap-14 mt-[-160px] md:mt-[-120px] lg:mt-[-20vh]">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-14 mt-[20vh] md:mt-[30vh]">
           <Image
             src={Logo}
             alt="Sculpted Audio Logo"
@@ -40,7 +40,7 @@ const Hero = () => {
           </Subheadline>
         </div>
         <div 
-        className="absolute left-1/2 transform -translate-x-1/2 translate-y-80 transition-opacity duration-300 ease-in-out"
+        className="absolute left-1/2 transform -translate-x-1/2 translate-y-48 lg:translate-y-80 transition-opacity duration-300 ease-in-out"
         style={{ opacity: arrowOpacity }}
       >
         <ChevronDown className="w-12 h-12 text-white animate-bounce" />
