b0a6bca86d1161d2e3454ccfd31e490bf77502b0
samsnwn <samuel.c.glantz@gmail.com>
2024-11-18 14:59:12 +0100

    textarea

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full border-y border-black bg-white px-3 py-2 text-sm placeholder:text-slate-500 focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[80px] rounded-none w-full border-y border-black bg-white px-3 py-2 text-sm placeholder:text-slate-500 focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
